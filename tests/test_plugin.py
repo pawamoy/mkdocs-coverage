7517543108bc133afe373d7383e5ffb63897a0fa
Timothée Mazzucotelli <pawamoy@pm.me>
2021-05-08 20:32:05 +0200

    tests: Fix tests (tests files also appear in coverage report now)

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -13,7 +13,7 @@ def test_plugin():
     build(config)
     site_coverage_dir = Path(config["site_dir"]) / "coverage"
     for html_file in site_coverage_dir.iterdir():
-        if html_file.suffix == ".html" and html_file.name != "index.html":
+        if html_file.suffix == ".html" and html_file.name != "index.html" and "tests" not in html_file.name:
             text = html_file.read_text()
             assert not re.search("covcovindex", text)
             assert not re.search('href="index.html"', text)

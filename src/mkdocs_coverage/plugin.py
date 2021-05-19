5519c1352759f36b5ff3e1f800ac41fd12cd4acb
Timothée Mazzucotelli <pawamoy@pm.me>
2021-05-19 20:35:08 +0200

    fix: Reset iframe height between page changes
    Issue #1: https://github.com/pawamoy/mkdocs-coverage/issues/1

--- src/mkdocs_coverage/plugin.py
+++ src/mkdocs_coverage/plugin.py
@@ -53,13 +53,20 @@ class MkDocsCoveragePlugin(BasePlugin):
             }
             </style>
 
+            <iframe id="coviframe" src="covindex.html" frameborder="0" scrolling="no" onload="resizeIframe();" width="100%"></iframe>
+
             <script>
-            function resizeIframe(obj) {
-                obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+            var coviframe = document.getElementById("coviframe");
+
+            function resizeIframe() {
+                coviframe.style.height = coviframe.contentWindow.document.documentElement.offsetHeight + 'px';
+            }
+
+            coviframe.contentWindow.document.body.onclick = function() {
+                coviframe.contentWindow.location.reload();
             }
             </script>
 
-            <iframe src="covindex.html" frameborder="0" scrolling="no" onload="resizeIframe(this)" width="100%"></iframe>
             """,
         )
         tempdir = mkdtemp()

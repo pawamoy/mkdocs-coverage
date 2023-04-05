9be474b850907b7e79e5cb800f2e52166a47e656
Timothée Mazzucotelli <pawamoy@pm.me>
2023-04-05 22:37:20 +0200

    chore: Template upgrade

--- src/mkdocs_coverage/__init__.py
+++ src/mkdocs_coverage/__init__.py
@@ -1,9 +1,8 @@
-"""
-MkDocs Coverage Plugin package.
+"""MkDocs Coverage Plugin package.
 
 MkDocs plugin to integrate your coverage HTML report into your site.
 """
 
 from __future__ import annotations
 
-__all__: list[str] = []  # noqa: WPS410 (the only __variable__ we use)
+__all__: list[str] = []

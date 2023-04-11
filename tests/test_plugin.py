4739d5e4e2174d014354181ee637defaf99e171b
Timothée Mazzucotelli <pawamoy@pm.me>
2023-04-11 11:42:25 +0200

    tests: Fix plugin test

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -10,7 +10,11 @@ from mkdocs.config.base import load_config
 def test_plugin() -> None:
     """Build our own documentation."""
     config = load_config()
-    build(config)
+    config["plugins"].run_event("startup", command="build", dirty=False)
+    try:
+        build(config)
+    finally:
+        config["plugins"].run_event("shutdown")
     site_coverage_dir = Path(config["site_dir"]) / "coverage"
     for html_file in site_coverage_dir.iterdir():
         if html_file.suffix == ".html" and html_file.name != "index.html" and "tests" not in html_file.name:
